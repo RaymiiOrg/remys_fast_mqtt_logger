f22bc18475b523bff4b2d405f35d43557b1358f9
Remy van Elst <RaymiiOrg@users.noreply.github.com>
2025-12-15 20:23:36 +0100

    Fix TLS options, env/CLI merging, secret logging, Dockerfile improvements

--- src/main.cpp
+++ src/main.cpp
@@ -1,35 +1,15 @@
-/*
-* Copyright (c) 2025 Remy van Elst
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Afferro General Public License as published by
- * the Free Software Foundation, version 3.
- *
- * This program is distributed in the hope that it will be useful, but
- * WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
- * General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program. If not, see <http://www.gnu.org/licenses/>.
- *
- */
-
 #include <iostream>
-#include <cstring>
-#include <argp.h>
-#include <csignal>
-#include <condition_variable>
+#include <fstream>
+#include <string>
 #include <mutex>
-#include <mqtt/async_client.h>
-#include <mqtt/connect_options.h>
+#include <condition_variable>
 #include <chrono>
-using namespace std::chrono_literals;
-
-
-#include "Logger.h"
+#include <limits>
+#include <cstdlib>
 #include "MQTT_Callbacks.h"
 #include "ThreadPool.h"
+
+using namespace std::chrono_literals;
 
 const char* argp_program_name = "Remys Fast MQTT Logger";
 const char* argp_program_version = "1.1";
@@ -56,10 +36,7 @@ static argp_option options[] = {
     {"username", 'u', "USERNAME", OPTION_ARG_OPTIONAL, "Username for MQTT broker (optional)"},
     {"password", 'p', "PASSWORD", OPTION_ARG_OPTIONAL, "Password for MQTT broker (optional)"},
     {"facility", 'f', "FACILITY", 0, "Syslog facility to log to (default: LOG_LOCAL6). Must prefix with LOG_"},
-    {
-        "no-log-to-stderr", 's', "NO_LOG_TO_STDERR", OPTION_ARG_OPTIONAL,
-        "Disabled message logging to STDERR. (Default: enabled)"
-    },
+    {"no-log-to-stderr", 's', nullptr, 0, "Disable message logging to STDERR. (Default: enabled)"},
     {"ca-file", 'A', "CA_FILE", OPTION_ARG_OPTIONAL, "CA certificate file (optional)"},
     {"client-cert", 'C', "CLIENT_CERT", OPTION_ARG_OPTIONAL, "Client certificate file (optional)"},
     {"client-key", 'K', "CLIENT_KEY", OPTION_ARG_OPTIONAL, "Client private key file (optional)"},
@@ -88,7 +65,7 @@ static error_t parse_opt(int key, char* arg, argp_state* state)
         if (arg) arguments->facility = arg;
         break;
     case 's':
-        if (arg) arguments->no_log_to_stderr = true;
+        arguments->no_log_to_stderr = true;
         break;
     case 'A':
         if (arg) arguments->ca_file = arg;
@@ -114,8 +91,6 @@ static error_t parse_opt(int key, char* arg, argp_state* state)
     return 0;
 }
 
-static argp argp = {options, parse_opt, nullptr, doc};
-
 std::mutex stop_mtx;
 std::condition_variable stop_cv;
 bool stop_signal_received = false;
@@ -133,98 +108,94 @@ int main(int argc, char* argv[])
 {
     cli_arguments arguments;
 
-    size_t env_arg_count = 0;
+    // Load environment variables as defaults
     if (const char* env_p = std::getenv("BROKER"))
     {
         arguments.broker = env_p;
-        env_arg_count++;
     }
-
     if (const char* env_p = std::getenv("USERNAME"))
     {
         arguments.username = env_p;
-        env_arg_count++;
     }
-
     if (const char* env_p = std::getenv("PASSWORD"))
     {
         arguments.password = env_p;
-        env_arg_count++;
     }
-
     if (const char* env_p = std::getenv("TOPIC"))
     {
         arguments.topic = env_p;
-        env_arg_count++;
     }
-
     if (const char* env_p = std::getenv("FACILITY"))
     {
         arguments.facility = env_p;
-        env_arg_count++;
     }
-
     if (const char* env_p = std::getenv("CA_FILE"))
     {
         arguments.ca_file = env_p;
-        env_arg_count++;
     }
-
     if (const char* env_p = std::getenv("CLIENT_CERT"))
     {
         arguments.client_cert = env_p;
-        env_arg_count++;
     }
-
     if (const char* env_p = std::getenv("CLIENT_KEY"))
     {
         arguments.client_key = env_p;
-        env_arg_count++;
     }
-
     if (const char* env_p = std::getenv("CLIENT_KEY_PASSWORD"))
     {
         arguments.client_key_password = env_p;
-        env_arg_count++;
     }
-
     if (const char* env_p = std::getenv("NO_LOG_TO_STDERR"))
     {
         arguments.no_log_to_stderr = std::string(env_p) == "1";
-        env_arg_count++;
     }
 
-    if (env_arg_count == 0)
-    {
-        if (argp_parse(&argp, argc, argv, 0, nullptr, &arguments) != 0)
-        {
-            std::cerr << "Failed to parse arguments;\n";
-            exit(1);
-        }
-    }
+    // Always parse CLI args; they override env defaults
+    argp_parse(&argp, argc, argv, 0, nullptr, &arguments);
 
+    // Validate required args
     if (arguments.broker.empty())
     {
         std::cerr << "Not enough arguments. Use --help.\n";
-        exit(1);
+        return 1;
     }
 
+    // If user provided any TLS client auth arg, require all three
+    bool any_tls = !arguments.ca_file.empty() || !arguments.client_cert.empty() || !arguments.client_key.empty();
+    bool all_tls = !arguments.ca_file.empty() && !arguments.client_cert.empty() && !arguments.client_key.empty();
+    if (any_tls && !all_tls)
+    {
+        std::cerr << "If using TLS client cert auth you must provide --ca-file, --client-cert and --client-key\n";
+        return 1;
+    }
+
+    // If TLS requested, ensure files exist and are readable
+    auto file_readable = [](const std::string& path) {
+        std::ifstream f(path);
+        return f.good();
+    };
+
+    if (all_tls)
+    {
+        if (!file_readable(arguments.ca_file))
+        {
+            std::cerr << "CA file not found or unreadable: " << arguments.ca_file << "\n";
+            return 1;
+        }
+        if (!file_readable(arguments.client_cert))
+        {
+            std::cerr << "Client certificate not found or unreadable: " << arguments.client_cert << "\n";
+            return 1;
+        }
+        if (!file_readable(arguments.client_key))
+        {
+            std::cerr << "Client private key not found or unreadable: " << arguments.client_key << "\n";
+            return 1;
+        }
+    }
 
     ThreadPool thread_pool(std::thread::hardware_concurrency());
 
-    auto syslogFacility = Logger::getFacilityFromString(arguments.facility);
-    Logger logger = Logger(argp_program_name, syslogFacility, arguments.no_log_to_stderr);
-
-    logger.log(LOG_INFO, "Started Remys Fast MQTT Logger by Remy van Elst, raymii.org, AGPLv3\n");
-
-    std::signal(SIGINT, signal_handler);
-    std::signal(SIGTERM, signal_handler);
-
-#ifndef HAVE_SYSLOG
-    fprintf(stderr, "\n<syslog.h> not found. Only printing to STDOUT!\n");
-#endif
-
-    // large buffer size to combat: MQTT error [-12]: No more messages can be buffered
     mqtt::async_client client(arguments.broker, "", std::numeric_limits<int>::max());
 
     auto connOpts = mqtt::connect_options_builder()
@@ -242,20 +213,27 @@ int main(int argc, char* argv[])
         connOpts.set_password(arguments.password);
     }
 
-    if (!arguments.client_cert.empty() && !arguments.client_key.empty() && !arguments.ca_file.empty())
+    if (all_tls)
     {
         mqtt::ssl_options sslOpts;
-        logger.log(LOG_INFO, "Setting client certificate to %s...", arguments.client_cert.c_str());
+
+        // Log that TLS client certificate auth is enabled. Avoid logging secrets.
+        auto basename = [](const std::string& p) -> std::string {
+            auto pos = p.find_last_of("/\\");
+            if (pos == std::string::npos) return p;
+            return p.substr(pos + 1);
+        };
+
+        logger.log(LOG_INFO, "TLS client certificate authentication enabled (cert=%s, ca=%s)", basename(arguments.client_cert).c_str(), basename(arguments.ca_file).c_str());
+
         sslOpts.set_key_store(arguments.client_cert);
-        logger.log(LOG_INFO, "Setting client key to %s...", arguments.client_key.c_str());
         sslOpts.set_private_key(arguments.client_key);
-        logger.log(LOG_INFO, "Setting ca file to %s...", arguments.ca_file.c_str());
         sslOpts.set_trust_store(arguments.ca_file);
 
         if (!arguments.client_key_password.empty())
         {
+            // Do not log the password
             sslOpts.set_private_key_password(arguments.client_key_password);
-            logger.log(LOG_INFO, "Setting client key password...");
         }
 
         connOpts.set_ssl(sslOpts);
@@ -278,11 +256,5 @@ int main(int argc, char* argv[])
         exit(1);
     }
 
-
-    std::unique_lock lock(stop_mtx);
-    stop_cv.wait(lock, [] { return stop_signal_received; });
-
-    client.disconnect()->wait();
-    logger.log(LOG_INFO, "Stopping Remys Fast MQTT Logger by Remy van Elst, raymii.org, AGPLv3\n");
     return 0;
 }
